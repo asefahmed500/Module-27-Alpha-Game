d374b187069c5a2c244e7adb1b25d617ec84b3cf
Asef Ahmed <asefahmed500@gmail.com>
2024-02-16 17:40:27 +0600

    home and Play section done

---
+++ script/alpha.js
@@ -0,0 +1,21 @@
+// function play(){
+
+//     // step 1 
+
+//     const homesection = document.getElementById('home');
+//     homesection.classList.add('hidden');
+
+//     // show the playground 
+//     const playgroundsection = document.getElementById('Playground');
+//     playgroundsection.classList.remove('hidden')
+  
+// }
+
+function play(){
+
+    hiddenElementByID('home')
+    showElementByID('Playground')
+}
+
+
+
---
+++ script/show.js
@@ -0,0 +1,9 @@
+function hiddenElementByID(elementID){
+    const element = document.getElementById(elementID);
+    element.classList.add('hidden')
+}
+
+function showElementByID(elementID){
+    const element = document.getElementById(elementID);
+    element.classList.remove('hidden')
+}
